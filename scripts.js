1f81b6f808df59b68b79ddb66e230a0cd299f348
Faheem <faheemmbasheer@gmail.com>
2024-09-23 10:14:32 +0530

    make the game graphical

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 let choices = ["rock", "paper", "scissors"];
 
+let humanPoint = 0;
+let computerPoint = 0;
+
+// selecting "results" div for playRound function to print results
+const results = document.querySelector("#results");
+
 function getComputerChoice() {
     // Get computer choice
     //  - Use Math.random() to select "rock", "paper" or "scissors"
@@ -19,54 +25,42 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getHumanChoice() {
-    // Get input from user
-    let humanChoice = prompt("Enter choice (rock, paper or scissors)");
-    // Converting input to lowercase
-    humanChoice = humanChoice.toLowerCase();
-    // Keep asking for correct input until the user enters a valid input
-    while (!(choices.includes(humanChoice))) {
-        alert("Invalid choice!")
-        humanChoice = prompt("Enter choice (rock, paper or scissors)");
-    }
-    return humanChoice;
-}
-
-function playRound(computerChoice, humanChoice) {
+function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
     switch(computerChoice) {
         case humanChoice:
-            console.log(`Thats a draw!`);
+            results.textContent = `Thats a draw!`;
             return "draw";
 
         case "rock":
             switch(humanChoice) {
                 case "paper":
-                    console.log(`You win!. ${humanChoice} beats ${computerChoice}.`);
+                    results.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
                     return "human";
                 case "scissors":
-                    console.log(`You lose!. ${computerChoice} beats ${humanChoice}.`);
+                    results.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
                     return "computer";
             }
             break;
 
         case "scissors":
             switch(humanChoice) {
-                case "paper":
-                    console.log(`You lose!. ${computerChoice} beats ${humanChoice}.`);
-                    return "computer";
                 case "rock":
-                    console.log(`You win!. ${humanChoice} beats ${computerChoice}.`);
+                    results.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
                     return "human";
+                case "paper":
+                    results.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
+                    return "computer";
             }
             break;
 
         case "paper":
             switch(humanChoice) {
                 case "scissors":
-                    console.log(`You win!. ${humanChoice} beats ${computerChoice}.`);
+                    results.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
                     return "human";
                 case "rock":
-                    console.log(`You lose!. ${computerChoice} beats ${humanChoice}.`);
+                    results.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
                     return "computer";
             }
             break;
@@ -74,28 +68,36 @@ function playRound(computerChoice, humanChoice) {
     }
 }
 
+const finalResult = document.querySelector("#finalResult");
 
-function playGame() {
-    let computerScore = 0;
-    let humanScore = 0;
-    let winner;
-    for (let i = 0; i < 5; i++) {
-        winner = playRound(getComputerChoice(), getHumanChoice());
-        if (winner === "human") {
-            humanScore++;
-        } else if (winner === "computer") {
-            computerScore++;
-        }
+const scores = document.querySelector("#scores");
+
+const bContainer = document.querySelector("#buttonContainer");
+// Making use of event delegation
+bContainer.addEventListener("click", function(event){
+    const target = event.target;
+    switch(playRound(target.id)) { 
+        // playRound returns either "human" or "computer"
+        case "human":
+            humanPoint++;
+            break;
+        case "computer":
+            computerPoint++;
+            break;
     }
 
-    console.log(`Your score: ${humanScore}\nComputer score: ${computerScore}`);
-    if (humanScore === computerScore) {
-        console.log("Thats a draw...");
-    } else if (humanScore > computerScore) {
-        console.log("You win!");
+    scores.textContent = `YOU: ${humanPoint}\nCOMPUTER: ${computerPoint}`;
+
+    if (humanPoint === 5) {
+        finalResult.textContent = "You Win!!!";
+        humanPoint = 0;
+        computerPoint = 0;
+        
+    } else if (computerPoint === 5) {
+        finalResult.textContent = "You lose :(";
+        humanPoint = 0;
+        computerPoint = 0;
     } else {
-        console.log("You lose...");
+        finalResult.textContent = "";
     }
-}
-
-playGame();
+});
